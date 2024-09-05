170bc62d2a9dddc4fe9f974bdafe92e91efc5c33
saisathwik22 <reddysaisathwik022@gmail.com>
2024-09-05 09:32:00 +0530

    Basic file configs CodeStage - 14:00

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/provider";
+import { mode } from "@chakra-ui/theme-tools";
+import { ColorModeScript, extendTheme } from "@chakra-ui/react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode("gray.800", "whiteAlpha.900")(props),
+      bg: mode("gray.100", "#101010")(props),
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
+
+const colors = {
+  gray: {
+    light: "#616161",
+    dark: "1e1e1e",
+  },
+};
+
+const theme = extendTheme({ config, styles, colors });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
