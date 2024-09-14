195a289d8ae0994524094890bc168ca649325575
saisathwik22 <reddysaisathwik022@gmail.com>
2024-09-14 22:30:26 +0530

    controller, route, model files setup. CodeStage - 2:09:00

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -8,6 +11,14 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // parse incoming data from req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data into req.body
+// extended: true --> even if req.body has some nested objects it'll be able to parse it without an issue.
+app.use(cookieParser());
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
